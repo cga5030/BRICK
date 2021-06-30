bde8c68dc8858b81a266670999194eb054ec2449
Carl Fredrick G. Aquino <51449688+cga5030@users.noreply.github.com>
2021-06-29 21:42:30 -0400

    added configuration for standard runs
    in the configuration section, I added lines to account for a 'standard' calibration scenario which runs the calibration using standard priors and data without any expert information, but with the expert forcing scenario (bamber et al 2019 high or low temperature scenario).

--- calibration/CARL_calib_driver.R
+++ calibration/CARL_calib_driver.R
@@ -3,18 +3,23 @@
 rm(list=ls())                        # Clear all previous variables
 
 ## Set up MCMC stuff here so that it can be automated for HPC
-nnode_mcmc000 <- 1
-niter_mcmc000 <- 2e7 # 1e6 for BRICK, 1e5 for DAIS
+nnode_mcmc000 <- 4
+niter_mcmc000 <- 2e5 # 1e6 for BRICK, 1e5 for DAIS
 gamma_mcmc000 <- 0.51    # rate of adaptation (between 0.5 and 1, lower is faster adaptation)
 accept_mcmc000 <- 0.15  # changed to 0.15 to help the sampler explore the space more carefully
                         # from Tony's 0.234: "Optimal as # parameters->infinity (Gelman et al, 1996; Roberts et al, 1997)" (Wong et al., 2017)
-experts=TRUE # do you wish to invert expert assessment?
+experts=FALSE # do you wish to invert expert assessment?
 alldata=TRUE # do you wish to invert paleo and instrumental data?
 
 if(alldata){
+  if(experts){
   configure <- '_Complete_0614_2e7_' # configure for your run
+  }
+  else{
+    configure <- '_Standard_0614_1e5_' # configure for your run
+  }
 } else{
-    configure <- '_Expert_0614_2e7_' # configure for your run
+    configure <- '_Expert_0614_4e7_' # configure for your run
   }
 
 ## Show plots? (probably want FALSE on HPC, non-interactive)
@@ -626,7 +631,7 @@ saveRDS(amcmc.out,file <- filename.RDS)
 ## Save workspace image - you do not want to re-simulate all those!
 save.image(file=filename.saveprogress)
 time.elapsed <- t.end - t.beg
-print("line 629")
+print("line 634")
 print(time.elapsed)
 
 
@@ -699,7 +704,7 @@ for (pp in 1:length(parnames)){
 t.end <- proc.time()
 time.elapsed <- t.end - t.beg
 save.image(file=filename.saveprogress)
-print("line 702")
+print("line 707")
 print(time.elapsed)
 print("TEDDY IS A CUTE PUPPER")
     
